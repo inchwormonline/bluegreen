fef439be93ac85827913f9b5d17cddf860bae8f0
Inchworm <faranak.heydarian@gmail.com>
2019-04-04 14:32:38 +0200

    Update Color Codes
    making colors slightly lighter

--- image.php
+++ image.php
@@ -6,11 +6,11 @@ $white = imagecolorallocate($im, 255, 255, 255);
 $deployment = getenv("COLOR");
 
 if ( $deployment == 'blue') {
-  $color = imagecolorallocate($im, 0, 0, 255);
+  $color = imagecolorallocate($im, 0, 0, 250);
 } elseif ($deployment == 'green')  {
-  $color = imagecolorallocate($im, 0, 255, 0);
+  $color = imagecolorallocate($im, 0, 250, 0);
 } else {
-  $color = imagecolorallocate($im, 0, 0, 255);
+  $color = imagecolorallocate($im, 0, 0, 250);
 }
 
 // Draw a filled rectangle
